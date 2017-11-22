4d134f1fcdf6ebcf2b8fe332f08955ea014a8e79
darkblank <darkblank1990@gmail.com>
2017-11-22 17:30:48 +0900

    reservation 모델 추가(미완성)

--- zinzi/reservations/models.py
+++ zinzi/reservations/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# fixme
+class Reservation(models.Model):
+    requested_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    reserved_date = models.DateTimeField()
+    payment_date = models.DateTimeField()
+    price = models.PositiveIntegerField(
+        max_length=8,
+    )
+    party = models.PositiveIntegerField()
+    reservation_number = models.PositiveIntegerField(
+        unique=True,
+        max_length=14,
+    )
+    is_confirmed = models.BooleanField(
+        default=False,
+    )
