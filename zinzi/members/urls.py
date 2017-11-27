de5b156c2df938092bea9aba091e65c5f8fbb187
Booki Park <bkbkgg@gmail.com>
2017-11-27 17:03:59 +0900

    profile에 user_pk 제거

--- zinzi/members/urls.py
+++ zinzi/members/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 urlpatterns = [
     # url(r'^/signup/$', ),
     # url(r'^/signin/$', ),
-    # url(r'/(?P<user_pk>\d+)/profile/$', ),
+    # url(r'^/profile/$', ),
     # url(r'/(?P<user_pk>\d+)/my-reservation/$' ),
 ]
