59607f3f1314cf4b5dfaab67e413cb20327bd9a7
isaccchoi <isaccchoi@naver.com>
2017-11-29 17:10:22 +0900

    restaurant detailview 작성, permissionclass 작성

--- zinzi/restaurants/views.py
+++ zinzi/restaurants/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Restaurant
 from .pagination import RestaurantListPagination
+from .permissions import IsOwnerOrReadOnly
 from .serializers import RestaurantSerializer
 
 
@@ -13,3 +14,11 @@ class RestaurantListView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # fixme - UserSerailizer가 없어 일단 request.user로 저장
         serializer.save(owner=self.request.user)
+
+
+class RestaurantDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    # 로그인을 하지 않았거나 Owner와 request.user가 같지 않을 경우 ReadOnly만 가능
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
