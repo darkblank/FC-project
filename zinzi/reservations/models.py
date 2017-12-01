f274504aae384225e53473cdc6b0507f3416b912
darkblank <darkblank1990@gmail.com>
2017-12-01 17:57:29 +0900

    Payment 모델 작성

--- zinzi/reservations/models.py
+++ zinzi/reservations/models.py
@@ -29,5 +29,26 @@ class Reservation(models.Model):
     )
 
 
-class TestModel(models.Model):
-    imp_uid = models.TextField()
+class Payment(models.Model):
+    # 고유번호
+    imp_uid = models.CharField(max_length=50)
+    merchant_uid = models.CharField(max_length=50)
+    # 지불방법 및 pg사 정보
+    pay_method = models.CharField(max_length=20)
+    pg_provider = models.CharField(max_length=20)
+    pg_tid = models.CharField(max_length=50)
+    # 주문정보
+    name = models.CharField(max_length=50)
+    amount = models.IntegerField()
+    cancel_amount = models.IntegerField()
+    currency = models.CharField(max_length=5)
+    status = models.CharField(max_length=10)
+    paid_at = models.IntegerField()
+    failed_at = models.IntegerField()
+    cancelled_at = models.IntegerField()
+    fail_reason = models.CharField(max_length=255)
+    cancel_reason = models.CharField(max_length=255)
+    # 주문자 정보
+    buyer_name = models.CharField(max_length=10)
+    buyer_email = models.CharField(max_length=30)
+    buyer_tel = models.CharField(max_length=20)
--- zinzi/reservations/views.py
+++ zinzi/reservations/views.py
@@ -9,7 +9,7 @@ class TestList(APIView):
     def post(self, request):
         iamport = Iamport(imp_key='6343293486082258',
                           imp_secret='JEAB6oXOMsc2oysgdu4tJzlfgQvn5sfP7Qqefn21Qe3fNwv11zuL9Q0qGvNMY2B6T1l8pn9fCdvpK0rL')
-        response = iamport.find(imp_uid=request.data.get('test_field'))
+        response = iamport.find(imp_uid=request.data.get('imp_uid'))
         serializer = TestSerializer(data=response)
         if serializer.is_valid():
             serializer.save()
