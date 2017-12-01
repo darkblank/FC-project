03271d781f461f3ccc8ed56252af6f70b83518e7
Booki Park <bkbkgg@gmail.com>
2017-12-01 19:34:56 +0900

    reservations app 활설화

--- zinzi/reservations/models.py
+++ zinzi/reservations/models.py
@@ -1,29 +1,29 @@
-# from django.db import models
-#
-# from members.models import User
-# from restaurants.models import ReservationInfo
-#
-#
-# # fixme
-# class Reservation(models.Model):
-#     user = models.ForeignKey(
-#         User,
-#         on_delete=models.PROTECT,
-#     )
-#     information = models.ForeignKey(
-#         ReservationInfo,
-#         on_delete=models.PROTECT,
-#     )
-#     requested_date = models.DateTimeField(
-#         auto_now_add=True,
-#     )
-#     reserved_date = models.DateTimeField()
-#     payment_date = models.DateTimeField()
-#     price = models.PositiveIntegerField()
-#     party = models.PositiveIntegerField()
-#     reservation_number = models.PositiveIntegerField(
-#         unique=True,
-#     )
-#     is_confirmed = models.BooleanField(
-#         default=False,
-#     )
+from django.db import models
+
+from members.models import User
+from restaurants.models import ReservationInfo
+
+
+# fixme
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+    )
+    information = models.ForeignKey(
+        ReservationInfo,
+        on_delete=models.PROTECT,
+    )
+    requested_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    reserved_date = models.DateTimeField()
+    payment_date = models.DateTimeField()
+    price = models.PositiveIntegerField()
+    party = models.PositiveIntegerField()
+    reservation_number = models.PositiveIntegerField(
+        unique=True,
+    )
+    is_confirmed = models.BooleanField(
+        default=False,
+    )
