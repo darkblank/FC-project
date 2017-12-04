d192b8424e5b4e68e6ce09c67f4572f2d8d4a6c4
darkblank <darkblank1990@gmail.com>
2017-12-04 22:32:05 +0900

    print문 삭제

--- zinzi/reservations/views.py
+++ zinzi/reservations/views.py
@@ -12,7 +12,6 @@ class PaymentList(APIView):
         iamport = Iamport(imp_key='6343293486082258',
                           imp_secret='JEAB6oXOMsc2oysgdu4tJzlfgQvn5sfP7Qqefn21Qe3fNwv11zuL9Q0qGvNMY2B6T1l8pn9fCdvpK0rL')
         response = iamport.find(imp_uid=request.data.get('imp_uid'))
-        print(response)
         serializer = PaymentSerializer(data=response)
         if serializer.is_valid():
             serializer.save()
