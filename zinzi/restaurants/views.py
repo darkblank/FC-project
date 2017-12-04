3a4e2c0966e8010ead7caef8accb6f166bf26968
isaccchoi <isaccchoi@naver.com>
2017-12-04 20:39:24 +0900

    RestaurantListView에 type 및 price filter 추가

--- zinzi/restaurants/views.py
+++ zinzi/restaurants/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 
 from .models import Restaurant
 from .pagination import RestaurantListPagination
@@ -7,9 +8,18 @@ from .serializers import RestaurantListSerializer, RestaurantDetailSerializer
 
 
 class RestaurantListView(generics.ListAPIView):
-    queryset = Restaurant.objects.all()
     serializer_class = RestaurantListSerializer
     pagination_class = RestaurantListPagination
+
+    # Querystring으로 전달된 type, price에 대해서 filter를 걸어 리턴하도록 설정
+    # fixme district 필터를 추가해야함
+    def get_queryset(self):
+        restaurant_type = self.request.query_params.get('type', None)
+        average_price = self.request.query_params.get('price', None)
+        if not restaurant_type or not average_price:
+            raise ValidationError
+        queryset = Restaurant.objects.filter(restaurant_type=restaurant_type, average_price=average_price)
+        return queryset
 
 
 class RestaurantDetailView(generics.RetrieveAPIView):
