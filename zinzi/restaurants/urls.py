860c73b1689b8b81f3bdac813ce5dbc35cc00bdc
isaccchoi <isaccchoi@naver.com>
2017-12-05 01:48:10 +0900

    CheckOpenedTimeView 추가 및 URL 연결

--- zinzi/restaurants/urls.py
+++ zinzi/restaurants/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import RestaurantListView, RestaurantDetailView
+from .views import RestaurantListView, RestaurantDetailView, CheckOpenedTimeView
 
 urlpatterns = [
     # /restaurants/
     url(r'^$', RestaurantListView.as_view(), name='restaurant-list'),
     # /restaurants/<pk>/
     url(r'(?P<pk>\d+)/$', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    url(r'check_opened_time/$', CheckOpenedTimeView.as_view(), name='check-time'),
 ]
--- zinzi/restaurants/views.py
+++ zinzi/restaurants/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics, permissions
 from rest_framework.exceptions import ValidationError
 
-from .models import Restaurant
+from .models import Restaurant, ReservationInfo
 from .pagination import RestaurantListPagination
 from .permissions import IsOwnerOrReadOnly
-from .serializers import RestaurantListSerializer, RestaurantDetailSerializer
+from .serializers import RestaurantListSerializer, RestaurantDetailSerializer, ReservationInfoSerializer
 
 
 class RestaurantListView(generics.ListAPIView):
@@ -17,7 +17,7 @@ class RestaurantListView(generics.ListAPIView):
         restaurant_type = self.request.query_params.get('type', None)
         average_price = self.request.query_params.get('price', None)
         if not restaurant_type or not average_price:
-            raise ValidationError
+            raise ValidationError('type 또는 price가 입력되지 않았습니다.')
         queryset = Restaurant.objects.filter(restaurant_type=restaurant_type, average_price=average_price)
         return queryset
 
@@ -28,3 +28,18 @@ class RestaurantDetailView(generics.RetrieveAPIView):
     # 로그인을 하지 않았거나 Owner와 request.user가 같지 않을 경우 ReadOnly만 가능
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
+
+
+class CheckOpenedTimeView(generics.ListAPIView):
+    serializer_class = ReservationInfoSerializer
+
+    def get_queryset(self):
+        res_pk = self.request.query_params.get('res_pk', None)
+        party = self.request.query_params.get('party', None)
+        date = self.request.query_params.get('date', None)
+        queryset = ReservationInfo.objects.filter(
+            restaurant_id=res_pk,
+            acceptable_size_of_party__gte=party,
+            date=date,
+        )
+        return queryset
