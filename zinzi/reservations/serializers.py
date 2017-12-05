15bf3c6e350e0ab1b64cf3dc86c7dacd9fefe9b3
darkblank <darkblank1990@gmail.com>
2017-12-05 21:37:53 +0900

    ReservationSerializer 추가

--- zinzi/reservations/serializers.py
+++ zinzi/reservations/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 
-from reservations.models import Payment
+from accounts.serializers import UserSerializer
+from reservations.models import Payment, Reservation
+from restaurants.serializers import ReservationInfoSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = (
-            '__all__'
+            '__all__',
+        )
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    information = ReservationInfoSerializer(read_only=True)
+
+    class Meta:
+        model = Reservation
+        fields = (
+            '__all__',
         )
