40eada4dbbaf4aaecfe171d42a98c1b15470034c
isaccchoi <isaccchoi@naver.com>
2017-12-08 16:47:15 +0900

    owener가 아닐경우 허용하지 않는 퍼미션 추가

--- zinzi/utils/permissions.py
+++ zinzi/utils/permissions.py
@@ -13,3 +13,10 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+
+
+class IsOwnerOrNotAllow(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser or request.user.is_staff:
+            return True
+        return obj.owner == request.user
