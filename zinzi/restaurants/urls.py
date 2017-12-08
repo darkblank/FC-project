bff949e41eedc6dacd24ffd17f69cef9c53f735d
isaccchoi <isaccchoi@naver.com>
2017-12-08 16:48:37 +0900

    StarRating에 0점 추가, restaurant management url 연결

--- zinzi/restaurants/urls.py
+++ zinzi/restaurants/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import url
 
 from .views import RestaurantListView, RestaurantDetailView, CheckOpenedTimeView, CommentListCreateView, \
-    CommentUpdateDestroyView
+    CommentUpdateDestroyView, ManagementRestaurant
 
 urlpatterns = [
     # /restaurants/
     url(r'^$', RestaurantListView.as_view(), name='restaurant-list'),
     # /restaurants/<pk>/
     url(r'^(?P<pk>\d+)/$', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    # /restaurants/<pk>/check_opened_time/?party=<int>&date=<date(YYYY-MM-DD)>
     url(r'^(?P<pk>\d+)/check_opened_time/$', CheckOpenedTimeView.as_view(), name='check-time'),
+    # /restaurants/<pk(comment pk)>/comments/
     url(r'^(?P<pk>\d+)/comments/$', CommentListCreateView.as_view(), name='comment-list-create'),
+    # /restaurants/comments/<pk(comment pk)>/
     url(r'^comments/(?P<pk>\d+)/$', CommentUpdateDestroyView.as_view(), name='comment-update-destroy'),
+    # /restaurants/management/<pk(restaurant pk)>/
+    url(r'^management/(?P<pk>\d+)/$', ManagementRestaurant.as_view(), name='management-restaurant'),
 ]
