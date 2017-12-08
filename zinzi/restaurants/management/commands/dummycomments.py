abce37bb53c8314f7b824190d7d3fad3324728ee
isaccchoi <isaccchoi@naver.com>
2017-12-09 01:47:11 +0900

    Dummy 생성 command 추가

--- zinzi/restaurants/management/commands/dummycomments.py
+++ zinzi/restaurants/management/commands/dummycomments.py
@@ -3,7 +3,7 @@ from random import randint
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
-from restaurants.models import Restaurant, Comment, STAR_RATING
+from restaurants.models import Comment, STAR_RATING, Restaurant
 
 User = get_user_model()
 user = User.objects.first()
@@ -11,12 +11,12 @@ user = User.objects.first()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for rest in Restaurant.objects.all():
-            for i in range(1, 7):
-                Comment.objects.create(
-                    author=user,
-                    restaurant=rest,
-                    star_rate=STAR_RATING[randint(1, len(STAR_RATING) - 1)][0],
-                    comment=f'Test Comment {rest.pk}'
-                )
-        print("Successfully create dummy Comments")
+        for i in range(1, 7):
+            Comment.objects.create(
+                author=user,
+                restaurant=Restaurant.objects.first(),
+                star_rate=STAR_RATING[randint(1, len(STAR_RATING) - 1)][0],
+                comment=f'Test Comment {rest.pk}'
+            )
+
+    print("Successfully create dummy Comments")
