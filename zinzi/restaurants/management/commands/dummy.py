3b7dfcb312cbc296f4bcf74d70e7059abbc5c977
isaccchoi <isaccchoi@naver.com>
2017-12-14 02:57:07 +0900

    dummy image uploade 성공, deploy후 테스트 필요

--- zinzi/restaurants/management/commands/dummy.py
+++ zinzi/restaurants/management/commands/dummy.py
@@ -1,8 +1,12 @@
+import urllib
 from datetime import datetime, timedelta
 from random import randint
 
-from django.contrib.auth import get_user_model
+import os
 from django.contrib.staticfiles.storage import staticfiles_storage as storage
+import requests
+from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.core.management import BaseCommand
 
 from restaurants.models import Restaurant, CHOICES_RESTAURANT_TYPE, CHOICES_PRICE, CHOICES_TIME, ReservationInfo, \
@@ -11,16 +15,21 @@ from restaurants.models import Restaurant, CHOICES_RESTAURANT_TYPE, CHOICES_PRIC
 User = get_user_model()
 user = User.objects.first()
 
-path = list()
-for i in range(1, 6):
-    path.append(storage.url('testimage/test' + str(i) + '.png'))
+base_location = storage.base_location
+location = '/testimage/test1.png'
+full_location = base_location + location
+image = open(full_location, 'r')
+
+
+img = File(open(os.path.join(storage.base_location + '/testimage/test1.png'), 'rb'))
+
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if Restaurant.objects.all().count() < 30:
+        if Restaurant.objects.all().count() < 50:
             for i in range(11):
-                restaurant = Restaurant.objects.create(
+                restaurant = Restaurant(
                     name='Dummy Restaurant' + str(i),
                     address='패스트캠퍼스',
                     district='강남구',
@@ -29,12 +38,13 @@ class Command(BaseCommand):
                     description='Dummy Restaurant description',
                     restaurant_type=CHOICES_RESTAURANT_TYPE[randint(0, len(CHOICES_RESTAURANT_TYPE) - 1)][0],
                     average_price=CHOICES_PRICE[randint(0, len(CHOICES_PRICE) - 1)][0],
-                    thumbnail=path[randint(0, 4)],
-                    menu=path[randint(0, 4)],
                     business_hours='Dummy Business hour',
                     maximum_party=randint(1, 100),
                     owner=user,
                 )
+                restaurant.thumbnail.save('testimage.png', img)
+                restaurant.menu.save('testimage.png', img)
+                restaurant.save()
                 for i in range(14):
                     for j in range(13):
                         if not ReservationInfo.objects.filter(restaurant=restaurant, time=CHOICES_TIME[j][0],
