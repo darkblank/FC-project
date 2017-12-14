05c1389d9aaf7de59efbac42e9d8a9669b6a44ba
Booki Park <bkbkgg@gmail.com>
2017-12-14 18:21:31 +0900

    이메일 인증, 비밀번호 수정 API 추가

--- zinzi/accounts/views.py
+++ zinzi/accounts/views.py
@@ -1,13 +1,20 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, status
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
 from rest_framework.compat import authenticate
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accounts.tokens import account_activation_token
 from .models import Profile
-from .serializers import SignupSerializer, UserSerializer, ProfileSerializer, PreferenceSerializer
+from .serializers import SignupSerializer, UserSerializer, ProfileSerializer, PreferenceSerializer, \
+    ChangePasswordSerializer
 
 User = get_user_model()
 
@@ -16,30 +23,45 @@ class Signup(APIView):
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            user = serializer.save()
+            serializer.save()
             # 이메일 인증을 할 시 True로 바뀜 (현재 기본값은 True)
-            user.is_active = True
-            user.save()
-            # current_site = get_current_site(request)
-            # mail_subject = '[Zinzi] 이메일 인증'
-            # message = render_to_string('<activation.html>', {
-            #     'user': user,
-            #     'domain': current_site.domain,
-            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            #     'token': account_activation_token.make_token(user),
-            # })
-            # to_email = serializer.validated_data['email']
-            # email = EmailMessage(
-            #     mail_subject,
-            #     message,
-            #     to=[to_email],
-            # )
-            # email.send()
-            Profile.objects.create(user=user)
+            user = serializer.data
+            current_site = get_current_site(request)
+            mail_subject = '[Zinzi] 이메일 인증'
+            message = render_to_string('activation.html', {
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = serializer.validated_data['email']
+            email = EmailMessage(
+                mail_subject,
+                message,
+                to=[to_email],
+            )
+            email.send()
             data = {
                 'user': serializer.data,
             }
             return Response(data=data, status=status.HTTP_201_CREATED)
+
+
+class Activation(APIView):
+    def get(self, request, uidb64, token):
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        decode_token = force_text(urlsafe_base64_decode(token))
+        user = User.objects.get(pk=uid)
+
+        if decode_token == request.user.token:
+            user.is_active = True
+            user.save()
+            Profile.objects.create(user=user)
+            data = {
+                'token': token,
+                'user': UserSerializer(user).data
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class Signin(APIView):
@@ -96,3 +118,35 @@ class PreferenceUpdate(generics.RetrieveUpdateAPIView):
     permission_classes = (
         IsAuthenticatedOrReadOnly,
     )
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+    )
+
+    def get_object(self):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            if not self.object.check_password(serializer.data.get('old_password')):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            if not serializer.validated_data['new_password'] == serializer.validated_data['new_password_confirm']:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            if serializer.validated_data['old_password'] == serializer.validated_data['new_password']:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            self.object.set_password(serializer.data.get('new_password'))
+            self.object.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordReset(APIView):
+    pass
