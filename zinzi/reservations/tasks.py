3fc668a6012599ade531370842a37811ca1494c4
darkblank <darkblank1990@gmail.com>
2017-12-19 07:10:46 +0900

    PaymentCreateView 에서 검증 실패시 이메일 보내주는 메서드 celery로 비동기 처리(local에서만 테스트 완료)

---
+++ zinzi/reservations/tasks.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+from config import celery_app
+
+
+@celery_app.task(bind=True)
+def send_mail_task(self, subject, message, recipient):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[
+            recipient,
+        ],
+    )
--- zinzi/reservations/views/payments.py
+++ zinzi/reservations/views/payments.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 
 from reservations.models import Reservation, Payment
 from reservations.serializers.payments import PaymentSerializer, PaymentCancelSerializer
+from reservations.tasks import send_mail_task
 
 User = get_user_model()
 
@@ -26,15 +27,10 @@ class PaymentCreateView(APIView):
         payment = iamport.find(imp_uid=request.data.get('imp_uid'))
         # 주문 해야할 금액과 실제 결제 금액이 일치하는지 검증 후 일치하지 않으면 자동으로 취소
         # 취소 되었을 경우에는 취소 되어진 결제정보로 데이터베이스에 저장
-        # 또한, 취소 되었을 경우 취소되었다는 메일 보냄(세팅에 이메일 세팅 추가하고 주석처리 제거), celery 이용하도록 변경 필요
+        # 또한, 취소 되었을 경우 취소되었다는 메일 보냄
         if not iamport.is_paid(int(request.data.get('price')), imp_uid=request.data.get('imp_uid')):
             cancel = iamport.cancel(u'가격 불일치', imp_uid=request.data.get('imp_uid'))
-            # send_mail(
-            #     subject='Hi',
-            #     message='Test',
-            #     from_email='darkblank1990@gmail.com',
-            #     recipient_list=['darkblank1990@gmail.com', ]
-            # )
+            send_mail_task.delay('Test', 'Test', 'darkblank1990@gmail.com')
             serializer = PaymentSerializer(data=cancel)
         else:
             serializer = PaymentSerializer(data=payment)
