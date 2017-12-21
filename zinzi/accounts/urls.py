15fb826271aa43a6bd10284025a58b68ce36562c
Booki Park <bkbkgg@gmail.com>
2017-12-21 18:01:33 +0900

    주석 추가

--- zinzi/accounts/urls.py
+++ zinzi/accounts/urls.py
@@ -3,14 +3,18 @@ from django.conf.urls import url
 from accounts import views
 
 urlpatterns = [
+    # Auth
     url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^signin/$', views.SigninView.as_view(), name='signin'),
     url(r'^signout/$', views.SignoutView.as_view(), name='signout'),
-    url(r'^(?P<pk>\d+)/profile/$', views.UpdateProfileView.as_view(), name='profile'),
     url(r'^(?P<pk>\d+)/change-password/$', views.ChangePasswordView.as_view(), name='change-password'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z_\-]+)/$', views.ActivateView.as_view(),
-        name='activate'),
+
+    # Activate
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z_\-]+)/$', views.activate, name='activate'),
     url(r'^withdraw/$', views.WithdrawView.as_view(), name='widthraw'),
+
+    # Profile
+    url(r'^(?P<pk>\d+)/profile/$', views.UpdateProfileView.as_view(), name='profile'),
     url(r'^(?P<pk>\d+)/owner-profile/$', views.OwnerProfileView.as_view(), name='owner-profile'),
     # url(r'^(?P<pk>\d+)/preference/$', views.PreferenceUpdate.as_view(), name='preference')
     # url(r'(?P<user_pk>\d+)/my-reservation/$' ),
