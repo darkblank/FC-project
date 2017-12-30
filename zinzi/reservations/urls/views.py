c2b1f4851879ae641fa89fa3e10388fe8f6b8f0b
darkblank <darkblank1990@gmail.com>
2017-12-30 20:10:41 +0900

    payment_reservation_save_view 시리얼라이저 사용 안하도록 변경, 템플릿에 csrf관련 문장 추가

--- zinzi/reservations/urls/views.py
+++ zinzi/reservations/urls/views.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from reservations.views.payment import payment_view, PaymentReservationsSaveView
+from reservations.views.payment import payment_view, payment_reservations_save_view
 from reservations.views.reservations import reservation_view
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', reservation_view, name='reservation'),
 
     url(r'^payment/$', payment_view, name='payment'),
-    url(r'^payment/complete/$', PaymentReservationsSaveView.as_view(), name='save_all'),
+    url(r'^payment/complete/$', payment_reservations_save_view, name='save_all'),
 ]
--- zinzi/reservations/views/payment.py
+++ zinzi/reservations/views/payment.py
@@ -1,15 +1,11 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
 from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from iamport import Iamport
-from rest_framework import exceptions
-from rest_framework.views import APIView
 
-from reservations.models import Reservation
-from reservations.serializers.payments import PaymentSerializer
+from reservations.models import Reservation, Payment
 from restaurants.models import ReservationInfo, Restaurant
 
 User = get_user_model()
@@ -42,15 +38,15 @@ def payment_view(request):
         return Http404
 
 
-class PaymentReservationsSaveView(APIView):
-    def post(self, request):
-        restaurant = int(request.data.get('restaurant'))
-        information = int(request.data.get('information'))
-        name = request.data.get('name')
-        party = int(request.data.get('party'))
-        phone_number = int(request.data.get('phone_number'))
-        email = request.data.get('email')
-        price = int(request.data.get('price'))
+def payment_reservations_save_view(request):
+    if request.method == 'POST':
+        restaurant = int(request.POST.get('restaurant'))
+        information = int(request.POST.get('information'))
+        name = request.POST.get('name')
+        party = int(request.POST.get('party'))
+        phone_number = int(request.POST.get('phone_number'))
+        email = request.POST.get('email')
+        price = int(request.POST.get('price'))
         # 유저 리퀘스트 유저로 바꿔줘야
         reservation = Reservation.objects.create(
             user=User.objects.first(),
@@ -65,20 +61,22 @@ class PaymentReservationsSaveView(APIView):
         iamport = Iamport(imp_key=settings.IMP_KEY,
                           imp_secret=settings.IMP_SECRET)
         # 입력한 imp_uid로부터 결제정보를 가져옴
-        payment = iamport.find(imp_uid=request.data.get('imp_uid'))
+        payment = iamport.find(imp_uid=request.POST.get('imp_uid'))
         # 주문 해야할 금액과 실제 결제 금액이 일치하는지 검증 후 일치하지 않으면 자동으로 취소
         # 취소 되었을 경우에는 취소 되어진 결제정보로 데이터베이스에 저장
         # 또한, 취소 되었을 경우 취소되었다는 메일 보냄(celery로 비동기 처리, celery config dev로 바꾸고 배포 관련 처리 하고 주석 빼야 함)
-        if not iamport.is_paid(int(request.data.get('price')), imp_uid=request.data.get('imp_uid')):
-            cancel = iamport.cancel(u'가격 불일치', imp_uid=request.data.get('imp_uid'))
+        all_fields = [f.name for f in Payment._meta.get_fields()]
+        if not iamport.is_paid(int(request.POST.get('price')), imp_uid=request.POST.get('imp_uid')):
+            cancel = iamport.cancel(u'가격 불일치', imp_uid=request.POST.get('imp_uid'))
             # send_mail_task.delay('Test', 'Test', 'darkblank1990@gmail.com')
-            serializer = PaymentSerializer(data=cancel)
+            Payment.objects.create(
+                **{key: value for key, value in cancel.items() if key in all_fields},
+                reservation=reservation
+            )
         else:
-            serializer = PaymentSerializer(data=payment)
-        if serializer.is_valid(raise_exception=True):
-            try:
-                serializer.save(reservation=reservation)
-            except IntegrityError:
-                raise exceptions.ValidationError
+            Payment.objects.create(
+                **{key: value for key, value in payment.items() if key in all_fields},
+                reservation=reservation
+            )
             return HttpResponse('success')
         return HttpResponse('failed')
