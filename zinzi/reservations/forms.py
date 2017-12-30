68bb875f1bcae6076e1fe386d9cace0680f5dc78
darkblank <darkblank1990@gmail.com>
2017-12-30 15:52:34 +0900

    현재 예약하고자 하는 레스토랑의 가능시간만 나오도록 forms 변경

--- zinzi/reservations/forms.py
+++ zinzi/reservations/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
 from reservations.models import Reservation
+from restaurants.models import ReservationInfo
 
 
 class ReservationForm(forms.ModelForm):
+    def __init__(self, restaurant, *args, **kwargs):
+        super(ReservationForm, self).__init__(*args, **kwargs)
+        self.fields['information'].queryset = ReservationInfo.objects.filter(restaurant=restaurant)
+
     class Meta:
         model = Reservation
         fields = (
