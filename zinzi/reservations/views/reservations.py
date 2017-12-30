8bc53a65623785c7fa1edb5261b4057102d9804d
darkblank <darkblank1990@gmail.com>
2017-12-30 13:39:07 +0900

    minor fix

--- zinzi/reservations/views/reservations.py
+++ zinzi/reservations/views/reservations.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
 from reservations.forms import ReservationForm
 from restaurants.models import Restaurant, ReservationInfo
