069f0579ee1f8fac87a1240e61acea209290a273
darkblank <darkblank1990@gmail.com>
2018-01-03 20:52:32 +0900

    취소정보 저장시 reservation.status가 request가 아닌 cancel request로 저장되게 변경

--- zinzi/reservations/views/payment.py
+++ zinzi/reservations/views/payment.py
@@ -106,7 +106,7 @@ def payment_cancel_view(request, pk):
         if form.is_valid():
             cancel = form.save(commit=False)
             cancel.payment = payment
-            payment.reservation.status = 'request'
+            payment.reservation.status = 'cancel request'
             payment.reservation.save()
             cancel.save()
         return redirect('index')
